87a6b82beb62b52f7ee81c275b90118bcb936251
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-06-14 23:40:08 +0200

    refactor(logger): simplify logger configuration by consolidating transport settings and adjusting log level based on environment

--- src/config/logger.dev.config.ts
+++ src/config/logger.dev.config.ts
@@ -2,35 +2,12 @@ import { Params } from 'nestjs-pino';
 
 export const loggerConfig: Params = {
   pinoHttp: {
-    customProps: () => ({
-      context: 'HTTP',
-    }),
     transport: {
-      targets: [
-        {
-          target: 'pino-pretty',
-          options: {
-            singleLine: true,
-            colorize: true,
-            levelFirst: true,
-            translateTime: 'HH:MM:ss Z',
-            ignore: 'pid,hostname'
-          }
-        },
-        {
-          target: 'pino/file',
-          options: {
-            destination: '/app/logs/app.log',
-            mkdir: true
-          }
-        }
-      ]
+      target: 'pino-pretty',
+      options: {
+        singleLine: true,
+      },
     },
-    level: 'debug',
-    autoLogging: true,
-    redact: {
-      paths: ['req.headers.authorization', 'req.headers.cookie'],
-      remove: true
-    }
-  }
+    level: process.env.NODE_ENV === 'test' ? 'silent' : 'debug',
+  },
 }; 
